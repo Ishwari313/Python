8a7d8d61fc4ebc54b5836fcebb8193497c74c50a
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2025-12-24 22:47:43 +0530

    Add files via upload

---
+++ IpoXX.py
@@ -0,0 +1,14 @@
+# How to accept the input
+
+print("Enter the First no")
+no1=input()
+
+print("Enter the second no")
+no2=input()
+
+ans=no1+no2
+print("Addition is:",ans)
+
+#input always gives the str as datatype
+print(type(no1)) #str
+print(type(no2)) 
