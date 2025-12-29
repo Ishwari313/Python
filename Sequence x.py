ab0668124da5a3e3b2fce559c07788e740954daa
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2025-12-29 23:04:19 +0530

    Add files via upload

---
+++ Sequence x.py
@@ -0,0 +1,16 @@
+#list (Frequently used) Duplicates allowed
+#set not contain duplicate value and it not maintain sequence of the data hence set not conatining the indexing
+
+value1=[10,20,30,40,10]
+value1[0] #10
+
+#tuple
+value2=(10,20,30,40,10)
+value2[0] #10
+#set
+value3={10,20,30,40,35}
+value3[0] #error
+
+
+
+#set not contain duplicate value and it not maintain sequence of the data
