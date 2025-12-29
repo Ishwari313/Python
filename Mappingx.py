4f360e81b8eaa20899ccfc8f6db33169f0ada31e
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2025-12-29 23:05:09 +0530

    Add files via upload

---
+++ Mappingx.py
@@ -0,0 +1,10 @@
+#dictionary
+#duplication key is not allowed but overrite with the new value
+information={"Name":"Rahul","Age":25,"city":"Pune","Marks":89.90,"city":"Mumbai"}
+print(information)
+
+#Access the key
+print(information["city"])
+
+information["age"]=27
+print(information)
