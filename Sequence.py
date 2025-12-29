e0b987ff90c0619ceec46a18aadaf8d470916f2c
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2025-12-29 23:03:46 +0530

    Add files via upload

---
+++ Sequence.py
@@ -0,0 +1,15 @@
+#list (Frequently used)
+value1=[10,20,30,40,10]
+print(value1)
+print(type(value1))
+
+#tuple
+value2=(10,20,30,40,10)
+print(value2)
+print(type(value2))
+
+#set
+value3={10,20,30,40,10}
+print(value3)
+print(type(value3))
+#set not contain duplicate value and it not maintain sequence of the data
