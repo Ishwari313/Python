a2a5a522d75f6ce7ca45825314ec287f63a3c651
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-07 23:19:03 +0530

    Add files via upload

---
+++ function_3.py
@@ -0,0 +1,23 @@
+'''1> very imp topic
+    2> Increase the reusability
+
+    two types user defined and in built
+
+    no any sq brackets
+
+    def is short form  of definition and it is coumplsory
+
+
+    :(colon) it is operator in python
+    at a time of defining the function and creating the loop
+'''
+print("Enter First number")
+no1 = int(input())
+
+print("Enter Second number")
+no2 = int(input())
+
+ans=no1*no2
+print("multiplication is:",ans)
+
+#this code is not a reusable
