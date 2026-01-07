d699283048abf30d5679cbe19fbefd33003d20e8
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-07 23:19:23 +0530

    Add files via upload

---
+++ function_4.py
@@ -0,0 +1,22 @@
+print("Enter First number")
+no1 = int(input())
+
+print("Enter Second number")
+no2 = int(input())
+
+ans=no1*no2
+print("multiplication is:",ans)
+
+#this code is not a reusable
+
+
+print("_"*40)
+
+print("Enter First number")
+no1 = int(input())
+
+print("Enter Second number")
+no2 = int(input())
+
+ans=no1*no2
+print("multiplication is:",ans)
