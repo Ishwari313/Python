9bb288ec771b7573b8dd3a96964ad26028148237
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-07 23:18:25 +0530

    Add files via upload

---
+++ function_1.py
@@ -0,0 +1,20 @@
+'''1> very imp topic
+    2> Increase the reusability
+
+    two types user defined and in built
+
+    no any sq brackets
+
+    def is short form  of definition and it is coumplsory
+
+
+    :(colon) it is operator in python
+    at a time of defining the function and creating the loop
+'''
+
+no1=10
+no2=11
+ans=no1*no2
+print("multiplication is:",ans)
+
+
