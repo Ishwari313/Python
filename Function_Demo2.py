006da10afbd6a23f7b1d41c776fce32a158ba1fb
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-08 22:40:05 +0530

    Add files via upload

---
+++ Function_Demo2.py
@@ -0,0 +1,12 @@
+# Empty Function
+
+def main():
+
+    pass  #it is a keyword to run the function becuase the function is empty it is always write in empty function
+
+#starter
+if __name__== "__main__":
+
+     main()
+   
+
