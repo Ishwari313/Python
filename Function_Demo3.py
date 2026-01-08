79cda370b197d83d15b46154c65f2249d7a54173
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-08 22:40:34 +0530

    Add files via upload

---
+++ Function_Demo3.py
@@ -0,0 +1,13 @@
+def main():
+
+    print("inside main")
+    pass  #it is a keyword to run the function becuase the function is empty it is always write in empty function
+
+#starter
+if __name__== "__main__":
+
+     main()
+
+     
+   
+'''pass always written in a such function which is always empty'''
