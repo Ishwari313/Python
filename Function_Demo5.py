f50d9e45ba1d4da534665f4b85fe86038ecc20ab
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-09 23:07:29 +0530

    Add files via upload

---
+++ Function_Demo5.py
@@ -0,0 +1,12 @@
+#Accept :One Parameter
+#eturn : nothing
+def marvalloues(value):
+    print("inside marvellous1:",value)
+
+def main():
+    marvalloues("python")
+    marvalloues(51)
+
+
+if __name__ == "__main__":
+    main()
