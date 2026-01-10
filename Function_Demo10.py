3e9e10e6d9a31b707fb46777d03e8bc165b40d25
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-10 21:20:30 +0530

    Add files via upload

---
+++ Function_Demo10.py
@@ -0,0 +1,16 @@
+#one Function can call another function
+
+def fun():
+    print("inside fun")
+
+def gun():
+    print("inside gun")
+    fun()
+
+def main():
+    gun()
+
+
+if __name__ == "__main__":
+    main()
+    
