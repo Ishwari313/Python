3efab31d12e8b09676e94de37d81b6f3c3de60ee
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-01-27 22:44:40 +0530

    Add files via upload

---
+++ ExceptionDemo3.py
@@ -0,0 +1,31 @@
+def main():
+    Ans = 0
+
+
+    try:
+        print("Inside Try:")
+        print("Enter first number")
+        No1 = int(input())
+
+        print("Enter second number")
+        No2 = int(input())
+
+        Ans = No1 / No2
+
+    except ZeroDivisionError as zobj:
+        print("inside except",zobj)
+
+    except ValueError as vobj:
+        print("Inside except:",vobj)
+
+    except Exception as eobj:
+        print("Inside except:",eobj)
+        
+
+    finally:
+        print("Inside finally")
+
+    print("Division is :",Ans)
+
+if __name__ =="__main__":
+    main()
