5362e81ef6ff45a0ce0445876c2165e77658e9e3
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-02-03 21:40:12 +0530

    Add files via upload

---
+++ BehavioursAll.py
@@ -0,0 +1,30 @@
+class Demo:
+    No = 10
+
+    #instance method
+    def __init__(self,A,B):
+        self.Value1 = A
+        self.Value2 = B
+
+    def fun(self):
+        print("Inside instance method fun:",self.Value1,self.Value2)
+
+    #class method does not need to obj for accessing
+    @classmethod  #coumplsory
+    def sun(cls):
+        print("Inside class method Sun:",cls.No)
+
+    @staticmethod # not coumplusery
+    def Gun():
+        print("Inside Static method Gun:",Demo.Gun)
+
+Demo.sun()
+print("class variable No : ",Demo.No)
+
+obj = Demo(11,21)
+
+obj.fun()
+
+print("instance variable :",obj.Value1,obj.Value2)
+
+Demo.Gun()
