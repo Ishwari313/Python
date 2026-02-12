1d6837eb866d1bae6ae874db8a994ff1aff6d669
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-02-12 23:42:33 +0530

    Add files via upload

---
+++ CharacteristicsAll.py
@@ -0,0 +1,16 @@
+class Demo:
+    No = 10
+
+    def __init__(self,A,B):
+        self.value1 = A
+        self.value2 = B
+
+print("class variable No:",Demo.No)
+
+obj1 = Demo(11,21)
+obj2 = Demo(51,101)
+
+
+print("intsance variable of obj1:",obj1.value1,obj1.value2)
+print("intsance variable of obj2:",obj2.value1,obj2.value2)
+
