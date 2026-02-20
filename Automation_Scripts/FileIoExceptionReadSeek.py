5cd68aabb14b3b24168744f5b17d115ea260675b
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-02-20 21:05:54 +0530

    Add files via upload

---
+++ Automation_Scripts/FileIoExceptionReadSeek.py
@@ -0,0 +1,28 @@
+def main():
+    try:
+        fobj=open("Hello.txt","r")
+        print("file gets Successfully open.")
+
+        print("Current offset is:",fobj.tell()) #0
+
+        fobj.seek(7)
+
+        print("Current offset is:",fobj.tell()) #7
+
+        Data =fobj.read(10)
+
+        print("Current offset is:",fobj.tell()) #17
+
+        print("Data form file is:",Data)
+
+        fobj.close()
+
+
+    except FileNotFoundError:
+        print("Unable to open as there is no such file.")
+
+    finally:
+        print("End of Application.")
+        
+if __name__ =="__main__":
+    main()
---
+++ Automation_Scripts/FileIoExceptionReadSeekXX.py
@@ -0,0 +1,34 @@
+# Seek(Offset(Kuthe),when(Kuthun))
+# Offset : 0 / 1 / 2
+# 0 : Starting
+# 1 : Current
+# 2 : End
+
+def main():
+    try:
+        fobj=open("Hello.txt","r")
+        print("file gets Successfully open.")
+
+        print("Current offset is:",fobj.tell()) #0
+
+        fobj.seek(6,1)
+
+        print("Current offset is:",fobj.tell()) #11
+
+        Data =fobj.read(10)
+
+        print("Current offset is:",fobj.tell()) #17
+
+        print("Data form file is:",Data)
+
+        fobj.close()
+
+
+    except FileNotFoundError:
+        print("Unable to open as there is no such file.")
+
+    finally:
+        print("End of Application.")
+        
+if __name__ =="__main__":
+    main()
