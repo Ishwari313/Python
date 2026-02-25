027bc9f7076714a4a384f638494b3d0842ed4024
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-02-25 21:43:31 +0530

    Add files via upload

---
+++ Automation_Scripts/DirectoryAutomationEmptyDelete.py
@@ -0,0 +1,44 @@
+import sys
+import os
+
+def DirectoryScanner(DirName ="Marvellous"):
+    Ret = False
+
+    Ret = os.path.exists(DirName)
+    if(Ret ==False):
+        print("There is no such directory")
+        return
+    
+    Ret = os.path.isdir(DirName)
+    if(Ret == False):
+        print("It is not a directory")
+        return
+    
+    for FolderName,SubFolder,FileName in os.walk(DirName):
+        for fName in FileName:
+            fName = os.path.join(FolderName,fName) # This line creates the full path of a file,
+                                                   # by combining the folder path and the file name.
+
+            print("File Name:",fName)
+            print("File Size :",os.path.getsize(fName)) 
+
+            if(os.path.getsize(fName)==0): #Empty file
+                os.remove(fName)
+
+def main():
+
+    Border ="-"*50
+    print(Border)
+    print("----------Marvellous Directory Automation---------")
+    print(Border)
+
+    if(len(sys.argv) != 2):
+        print("Invalid Number of arguments")
+        print("Please specify the name of directory")
+        return
+    
+    DirectoryScanner(sys.argv[1]) #call the function
+    
+
+if __name__ =="__main__":
+    main()
