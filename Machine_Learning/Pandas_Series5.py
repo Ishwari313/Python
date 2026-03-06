d4594665eb2953636eb30ca602f3d09be2ad71fa
Ishwari Khalkar <159133177+Ishwari313@users.noreply.github.com>
2026-03-06 23:13:58 +0530

    Add files via upload

---
+++ Machine_Learning/Pandas_Series5.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+def main():
+
+
+    sobj= pd.Series([25000,27000,29000,30000],index=["PPA","LB","PYTHON","REACT"])
+    print(sobj)
+
+    print(sobj["PYTHON"])
+
+if __name__ =="__main__":
+    main()
+
+# series should contain homogenous data
